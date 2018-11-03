a5af8ba8a8f53b0b01b6c852b1a727fd4b3cfaf2
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-03 19:05:20 +0000

    añadido task <css> a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
+    less = require('gulp-less');
 
 gulp.task('html', () =>  {
     return gulp.src('./src/*.html')
-           .pipe(gulp.dest('./dist'))
+           .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('css', () => {
+    return gulp.src('./src/css/*.less')
+            .pipe(less())
+            .pipe(gulp.dest('./dist/css'))
 });
